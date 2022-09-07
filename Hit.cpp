5ea7d2e130b70f05816f2ff5d7680d657357ec0e
etigoshun <k021g1375@g.neec.ac.jp>
2022-09-07 16:51:25 +0900

    当たり判定関数の追加

--- Hit.cpp
+++ Hit.cpp
@@ -1,1 +1,38 @@
 #include "Hit.h"
+
+//‰~‚Æ‹éŒ`‚Ì“–‚½‚è”»’è
+int BoxCircle(int LeftTopBoxX, int LeftTopBoxY, int RightBottomX, int RightBottomY, int circleX, int circleY, int r)
+{
+    if ((circleX > LeftTopBoxX - r)
+        && (circleX < RightBottomX + r)
+        && (circleY > LeftTopBoxY - r)
+        && (circleY < RightBottomY + r)) 
+    {
+        return 1;
+    }
+    else 
+    {
+        return 0;
+    }
+}
+
+//’ZŒ`‚Æ‹éŒ`‚Ì“–‚½‚è”»’è
+int BoxHit(int playerlefttopX, int playerlefttopY, int playerrightbottomX, int playerrightbottomY,
+	int enemylefttopX, int enemylefttopY, int enemyrightbottomX, int enemyrightbottomY)
+{
+	if (playerlefttopX < enemyrightbottomX && enemylefttopX < playerrightbottomX)
+	{
+		if (playerlefttopY < enemyrightbottomY && enemylefttopY < playerrightbottomY)
+		{
+			return 1;
+		}
+		else
+		{
+			return 0;
+		}
+	}
+	else
+	{
+		return 0;
+	}
+}
--- Hit.h
+++ Hit.h
@@ -1,5 +1,8 @@
 #pragma once
-class Hit
-{
-};
 
+//‰~‚Æ‹éŒ`‚Ì“–‚½‚è”»’è
+int BoxCircle(int LeftTopBoxX, int LeftTopBoxY, int RightBottomX, int RightBottomY, int circleX, int circleY, int r);
+
+//‹éŒ`‚Æ‹éŒ`‚Ì“–‚½‚è”»’è
+int BoxHit(int playerlefttopX, int playerlefttopY, int playerrightbottomX, int playerrightbottomY,
+	int enemylefttopX, int enemylefttopY, int enemyrightbottomX, int enemyrightbottomY);
