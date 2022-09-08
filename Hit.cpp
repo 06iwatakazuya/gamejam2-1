25652d135bca39375c5a47f288188de9438a06e3
OonisiHiroki <k021g1328@g.neec.ac.jp>
2022-09-08 15:07:46 +0900

    マップチップの当たり判定(左右)

--- Hit.cpp
+++ Hit.cpp
@@ -17,12 +17,12 @@ int BoxCircle(int LeftTopBoxX, int LeftTopBoxY, int RightBottomX, int RightBotto
 }
 
 //短形と矩形の当たり判定
-int BoxHit(int playerlefttopX, int playerlefttopY, int playerrightbottomX, int playerrightbottomY,
-	int enemylefttopX, int enemylefttopY, int enemyrightbottomX, int enemyrightbottomY)
+int BoxHit(int playerLeftTopX, int playerLeftTopY, int playerRightBottomX, int playerRightBottomY,
+		   int enemyLeftTopX, int enemyLeftTopY, int enemyRightBottomX, int enemyRightBottomY)
 {
-	if (playerlefttopX < enemyrightbottomX && enemylefttopX < playerrightbottomX)
+	if (playerLeftTopX < enemyRightBottomX && enemyLeftTopX < playerRightBottomX)
 	{
-		if (playerlefttopY < enemyrightbottomY && enemylefttopY < playerrightbottomY)
+		if (playerLeftTopY < enemyRightBottomY && enemyLeftTopY < playerRightBottomY)
 		{
 			return 1;
 		}
@@ -36,3 +36,49 @@ int BoxHit(int playerlefttopX, int playerlefttopY, int playerrightbottomX, int p
 		return 0;
 	}
 }
+
+//-------マップチップとプレイヤーの当たり判定-------//
+
+//左に向かった時のマップチップの当たり判定
+int leftHitMaptip(int playerPosX, int playerPosY, int playerRadius, int playerSpeed,
+			  int *leftTopX, int *leftTopY, int *leftBottomX, int *leftBottomY,
+			  int leftCenterX, int leftCenterY,
+			  int blockSize, int map) 
+{
+	*leftTopX = ((playerPosX - playerSpeed) - playerRadius) / blockSize;
+	*leftTopY = (playerPosY - playerRadius) / blockSize;
+	*leftBottomX = ((playerPosX - playerSpeed) - playerRadius) / blockSize;
+	*leftBottomY = (playerPosY - playerRadius) / blockSize;
+	leftCenterX = ((playerPosX - playerSpeed) - playerRadius) / blockSize;
+	leftCenterY = playerPosY / blockSize;
+	if (map[leftTopY][leftTopX] == 0 && map[leftBottomX][leftBottomY] == 0) 
+	{
+		playerPosX -= playerSpeed;
+	}
+}
+
+//右に向かった時のマップチップの当たり判定
+int rightHitMaptip(int playerPosX, int playerPosY, int playerRadius, int playerSpeed,
+				   int *rightTopX, int *rightTopY, int *rightBottomX, int *rightBottomY,
+				   int rightCenterX, int rightCenterY,
+				   int blockSize, int map) 
+{
+	*rightTopX = ((playerPosX - playerSpeed) - playerRadius) / blockSize;
+	*rightTopY = (playerPosX - playerRadius) / blockSize;
+	*rightBottomX = ((playerPosX - playerSpeed) - playerRadius) / blockSize;
+	*rightBottomY = (playerPosX - playerRadius) / blockSize;
+	rightCenterX = ((playerPosX - playerSpeed) - playerRadius) / blockSize;
+	rightCenterY = (playerPosX - playerRadius) / blockSize;
+	if (map[rightTopY][rightTopX] == 0 && map[rightBottomY][rightBottomX] == 0) 
+	{
+		playerPosX += playerSpeed;
+	}
+}
+
+//int underHitMaptip(int playerPosX, int playerPosY, int playerRadius, int playerSpeed,
+//				   int* rightBottomX, int* rightBottomY, int* leftBottomX, int* leftBottomY,
+//				   int underCenterX, int underCenterY,
+//				   int blockSize, int map) 
+//{
+//	*rightBottomX = 
+//}
--- Hit.h
+++ Hit.h
@@ -4,5 +4,17 @@
 int BoxCircle(int LeftTopBoxX, int LeftTopBoxY, int RightBottomX, int RightBottomY, int circleX, int circleY, int r);
 
 //矩形と矩形の当たり判定
-int BoxHit(int playerlefttopX, int playerlefttopY, int playerrightbottomX, int playerrightbottomY,
-	int enemylefttopX, int enemylefttopY, int enemyrightbottomX, int enemyrightbottomY);
+int BoxHit(int playerLeftTopX, int playerLeftTopY, int playerRightBottomX, int playerRightBottomY,
+	int enemyLeftTopX, int enemyLeftTopY, int enemyRightBottomX, int enemyRightBottomY);
+
+//左マップチップの当たり判定
+int leftHitMaptip(int playerPosX, int playerPosY, int playerRadius, int playerSpeed,
+				  int* leftTopX, int* leftTopY, int* leftBottomX, int* leftBottomY,
+				  int leftCenterX, int leftCenterY,
+				  int blockSize, int map);
+
+//右マップチップの当たり判定
+int rightHitMaptip(int playerPosX, int playerPosY, int playerRadius, int playerSpeed,
+				   int* rightTopX, int* rightTopY, int* rightBottomX, int* rightBottomY,
+				   int rightCenterX, int rightCenterY,
+				   int blockSize, int map);
