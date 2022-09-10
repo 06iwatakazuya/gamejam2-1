b214a66343d706017ee7b148cde625aeaa6c57a8
etigoshun <k021g1375@g.neec.ac.jp>
2022-09-10 17:12:39 +0900

    プレイヤーの攻撃処理の追加

--- Player.cpp
+++ Player.cpp
@@ -1,52 +1,152 @@
 #include "Player.h"
 #include "Dxlib.h"
 
+/// <summary>
+/// 初期化
+/// </summary>
 void Player::Initialize() {
 	playerAlive = 1;
 	playerPosX = 100;
 	playerPosY = 592;
-	playerRadius = 16;
+	playerRadiusX = 16;
+	playerRadiusY = 32;
 	playerSpeed = 10;
 
 	jumpFlag = 0;
 	jump = 15;
+
+	fireFlag = 0;
+	firePosX = playerPosX;
+	firePosY = playerPosY;
+	fireRadius = 5;
+	fireSpeed = 5;
+	fireGravity = -3;
+	/*for (std::unique_ptr<PlayerAttack>& fire : fire) {
+		fire->Initialize(playerPosX, playerPosY);
+	}*/
 }
 
-void Player::Update() {
-	// 最新のキーボード情報だったものは1フレーム前のキーボード情報として保存
-	for (int i = 0; i < 256; ++i) {
-		oldkeys[i] = keys[i];
-	}
-	// 最新のキーボード情報を取得
-	GetHitKeyStateAll(keys);
+/// <summary>
+/// 更新
+/// </summary>
+void Player::Update(char* keys, char* oldkeys)
+{
 
 	if (keys[KEY_INPUT_A] == 1) {
 		playerPosX -= playerSpeed;
 	}
-	else if (keys[KEY_INPUT_D] == 1) {
+	else if (keys[KEY_INPUT_D] == 1)
+	{
 		playerPosX += playerSpeed;
 	}
-	if (playerPosY >= 592) {
-		if (keys[KEY_INPUT_SPACE] == 1 && oldkeys[KEY_INPUT_SPACE] == 0) {
+	if (playerPosY >= 592)
+	{
+		if (keys[KEY_INPUT_SPACE] == 1 && oldkeys[KEY_INPUT_SPACE] == 0)
+		{
 			jump = -17;
 		}
 	}
 	playerPosY += jump;
-	jump++;
-	if (playerPosY >= 592) {
+	jump += 1;
+	if (playerPosY >= 592)
+	{
 		jump = 0;
 	}
+
+	if (keys[KEY_INPUT_A] == 1)
+	{
+		playerDirection = true;
+	}
+	else if (keys[KEY_INPUT_D] == 1)
+	{
+		playerDirection = false;
+	}
+
+	//プレイヤーの攻撃処理
+	Attack();
+
+	/*for (std::unique_ptr<PlayerAttack>& fire : fire) {
+		fire->Update();
+	}*/
 }
 
-void Player::Draw() {
-	if (playerAlive == 1) {
-		DrawBox(playerPosX - playerRadius,
-				playerPosY - playerRadius,
-				playerPosX + playerRadius,
-				playerPosY + playerRadius,
-				GetColor(200, 200, 200),
-				true);
+/// <summary>
+/// 描画
+/// </summary>
+void Player::Draw()
+{
+	if (playerAlive == 1)
+	{
+		DrawBox(playerPosX - playerRadiusX,
+			playerPosY - playerRadiusY,
+			playerPosX + playerRadiusX,
+			playerPosY + playerRadiusY,
+			GetColor(200, 200, 200),
+			true);
 	}
+
+	if (fireFlag == 1)
+	{
+		DrawCircle(firePosX, firePosY, fireRadius, GetColor(255, 50, 50), true);
+	}
+
+	/*for (std::unique_ptr<PlayerAttack>& fire : fire) {
+		fire->Draw();
+	}*/
+
 	DrawFormatString(50, 30, GetColor(255, 255, 255), "playerPosX : %d", playerPosX);
 	DrawFormatString(50, 50, GetColor(255, 255, 255), "playerPosY : %d", playerPosY);
+	DrawFormatString(50, 70, GetColor(255, 255, 255), "fireTimer : %d", fireTimer);
+	DrawFormatString(50, 90, GetColor(255, 255, 255), "fireFlag : %d", fireFlag);
+	DrawFormatString(50, 110, GetColor(255, 255, 255), "playerDirection : %d", playerDirection);
+
+}
+
+/// <summary>
+/// 攻撃
+/// </summary>
+void Player::Attack()
+{
+	if (fireFlag == 0)
+	{
+		firePosY = playerPosY;
+		firePosX = playerPosX;
+		fireGravity = -3;
+		if (firePosY <= 650)
+		{
+			fireTimer++;
+		}
+	}
+
+	if (fireTimer >= 50)
+	{
+		fireFlag = 1;
+		fireTimer = 0;
+	}
+
+	if (fireFlag == 1)
+	{
+		if (playerDirection == false)
+		{
+			firePosX += fireSpeed;
+			firePosY += fireGravity;
+			fireGravity++;
+		}
+		else if (playerDirection == true)
+		{
+			firePosX -= fireSpeed;
+			firePosY += fireGravity;
+			fireGravity++;
+		}
+	}
+
+	if (firePosY >= 650)
+	{
+		fireFlag = 0;
+	}
+
+	/*std::unique_ptr<PlayerAttack> newFire = std::make_unique<PlayerAttack>();
+	newFire->Initialize(playerPosX, playerPosY);
+
+	fire.push_back(std::move(newFire));*/
 }
--- Player.h
+++ Player.h
@@ -1,10 +1,33 @@
 #pragma once
+#include "PlayerAttack.h"
+#include <list>
+#include <memory>
+
 class Player
 {
 public:
+	/// <summary>
+	/// 初期化
+	/// </summary>
 	void Initialize();
-	void Update();
+
+	/// <summary>
+	/// 更新
+	/// </summary>
+	void Update(char* keys, char* oldkeys);
+
+	/// <summary>
+	/// 描画
+	/// </summary>
 	void Draw();
+
+	/// <summary>
+	/// 攻撃
+	/// </summary>
+	void Attack();
+
+	// 弾リスト取得
+	const std::list<std::unique_ptr<PlayerAttack>>& GetBullets() { return fire; }
 
 private:
 	/// <summary>
@@ -13,8 +36,10 @@ private:
 	int playerAlive = 1;
 	int playerPosX = 100;
 	int playerPosY = 700;
-	int playerRadius = 16;
+	int playerRadiusX = 16;
+	int playerRadiusY = 16;
 	int playerSpeed = 10;
+	bool playerDirection = 0;
 
 	/// <summary>
 	/// ジャンプ
@@ -22,10 +47,21 @@ private:
 	int jump;
 	int jumpFlag;
 
-	// 最新のキーボード情報用
-	char keys[256] = { 0 };
+	//const int
+	int fireFlag;
+	int firePosX;
+	int firePosY;
+	int fireRadius;
+	int fireSpeed;
+	int fireTimer;
+	int fireGravity;
 
-	// 1ループ(フレーム)前のキーボード情報
-	char oldkeys[256] = { 0 };
+	//// 最新のキーボード情報用
+	//char keys[256] = { 0 };
+
+	//// 1ループ(フレーム)前のキーボード情報
+	//char oldkeys[256] = { 0 };
+	// 弾
+	std::list<std::unique_ptr<PlayerAttack>> fire;
 };
 
