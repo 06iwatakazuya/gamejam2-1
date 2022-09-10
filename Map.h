a61aecf9daa88253f656a21cf732fe2bc0d6a960
etigoshun <k021g1375@g.neec.ac.jp>
2022-09-10 17:10:40 +0900

    マップの追加

--- Map.h
+++ Map.h
@@ -1,9 +1,40 @@
 #pragma once
+#include "DxLib.h"
+
 class Map 
 {
-public:
+public://メンバ関数
+
+	//マップシーン
+	enum class MapScene
+	{
+		NONE,		//0
+		BLOCK,		//1
+		ICEBLOCK,	//2
+		
+	};
+
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	void Initialize();
+
+	/// <summary>
+	/// 更新
+	/// </summary>
+	void Update();
+
+	/// <summary>
+	/// 描画
+	/// </summary>
+	void Draw();
 
 	//マップ追加
 	void CreateMap();
+
+private://メンバ変数
+
+	//マップシーン
+	int mapScene = 0;
 };
 
